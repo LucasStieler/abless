188684787ffafd094c8868e6d557f9c5ea70110e
LucasStieler <lucas.maximilian.stieler@gmail.com>
2024-12-22 12:13:45 +0100

    Initial Commit

---
+++ abless/ablessApp.swift
@@ -0,0 +1,17 @@
+//
+//  ablessApp.swift
+//  abless
+//
+//  Created by Lucas Maximilian Stieler on 22.12.24.
+//
+
+import SwiftUI
+
+@main
+struct ablessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ablessTests/ablessTests.swift
@@ -0,0 +1,17 @@
+//
+//  ablessTests.swift
+//  ablessTests
+//
+//  Created by Lucas Maximilian Stieler on 22.12.24.
+//
+
+import Testing
+@testable import abless
+
+struct ablessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
