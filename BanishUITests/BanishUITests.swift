ce4e7bb16fb12be03440e35c20dd276d59d3d35f
LucasStieler <lucas.maximilian.stieler@gmail.com>
2024-12-27 19:21:18 +0100

    test

---
+++ BanishUITests/BanishUITests.swift
@@ -0,0 +1,12 @@
+//
+//  BanishUITests.swift
+//  BanishUITests
+//
+//  Created by Lucas Maximilian Stieler on 22.12.24.
+//
+
+import XCTest
+
+final class BanishUITests: XCTestCase {
+    // ... rest of the file remains the same ...
+} 
---
+++ BanishUITests/BanishUITestsLaunchTests.swift
@@ -0,0 +1,12 @@
+//
+//  BanishUITestsLaunchTests.swift
+//  BanishUITests
+//
+//  Created by Lucas Maximilian Stieler on 22.12.24.
+//
+
+import XCTest
+
+final class BanishUITestsLaunchTests: XCTestCase {
+    // ... rest of the file remains the same ...
+} 
