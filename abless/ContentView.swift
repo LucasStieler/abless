d257c1cd14050c84b738cfc2a74f5b32d03fd9a1
LucasStieler <lucas.maximilian.stieler@gmail.com>
2024-12-27 17:34:59 +0100

    idk

--- abless/ContentView.swift
+++ abless/ContentView.swift
@@ -140,38 +140,6 @@ func reloadContentBlocker() {
     }
 }
 
-// Helper class for app detection
-class AppDetector {
-    private let appSchemes = [
-        "youtube": [
-            "youtube://",
-            "vnd.youtube://",
-            "youtube-app://",
-            "com.google.ios.youtube://"
-        ],
-        "instagram": [
-            "instagram://",
-            "instagram-stories://"
-        ],
-        "tiktok": [
-            "tiktok://",
-            "snssdk1233://"
-        ]
-    ]
-    
-    func hasConflictingApps() -> Bool {
-        for (_, schemes) in appSchemes {
-            for scheme in schemes {
-                if let url = URL(string: scheme),
-                   UIApplication.shared.canOpenURL(url) {
-                    return true
-                }
-            }
-        }
-        return false
-    }
-}
-
 #Preview {
     ContentView()
 }
