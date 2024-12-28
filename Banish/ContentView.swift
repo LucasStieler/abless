6f3ab57bb3b9751cf147edd2cd1afa0f47f18a68
LucasStieler <lucas.maximilian.stieler@gmail.com>
2024-12-28 12:51:47 +0100

    idk

--- Banish/ContentView.swift
+++ Banish/ContentView.swift
@@ -39,7 +39,7 @@ struct ContentView: View {
                                         removal: .move(edge: .leading).combined(with: .opacity)
                                     ))
                             } else {
-                                SafariLoginView(currentStep: $currentStep, isSetupComplete: true)
+                                HowItWorksView(currentStep: $currentStep)
                                     .transition(.asymmetric(
                                         insertion: .move(edge: .trailing).combined(with: .opacity),
                                         removal: .move(edge: .leading).combined(with: .opacity)
